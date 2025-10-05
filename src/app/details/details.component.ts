5c8c9cee607529bf82229df44dfbad9aee34770c
Meraj al Maksud <maksud.software@daffodilvarsity.edu.bd>
2025-10-05 15:40:43 +0600

    integrate angular forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,10 +2,13 @@ import {Component, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocation} from '../housing-location/housinglocation';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
@@ -16,8 +19,22 @@ export class DetailsComponent {
 
   housingLocation: HousingLocation | undefined;
 
+  // forms
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -119,4 +119,7 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+  }
 }
